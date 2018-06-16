53872e7a8d39b726759e95a7981a6a2a7624275d
Mac <mac@Macs-MacBook-Pro-2.local>
2018-06-16 01:45:28 +0100

    Add movie details page

--- routes/details.js
+++ routes/details.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+//API variables
+var apiUrl = 'http://api.douban.com/v2/movie/'
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with movie details');
+router.get('/:id', function(req, res, next) {
+
+	var id = req.params.id;
+
+	var detailMovie = apiUrl + 'subject/' + id;
+
+	request(detailMovie, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    //console.log(body) // Print the google web page.
+	    //console.log(body);
+	    res.render('details', { movie: JSON.parse(body) });
+	    //res.send(body);
+	  } else {
+	  	res.status(500).send('Oops ! Failed to request API data,');
+	  }
+	});
+
 });
 
 module.exports = router;
