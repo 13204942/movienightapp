9f5052e3273db359d4851f8eae21f97e6622f47b
Fangyijie Wang <fangyijie.wang@fleetmatics.com>
2018-06-22 15:41:55 +0100

    Implement search page and functionality

--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
-/* GET users listing. */
+//var key = 'apikey=0df993c66c0c636e29ecbb5344252a4a';
+var apiUrl = 'https://api.douban.com/v2/movie/search?q='
+
+//https://api.douban.com/v2/movie/search?q=张艺谋
+//https://api.douban.com/v2/movie/search?start=30&q=张艺谋
+
+/* GET movies listing. */
 router.get('/', function(req, res, next) {
-  //res.send('this is search page');
-  res.render('search');
+	
+	//decode Chinese characters or Russian characters
+  var movieParameter = encodeURIComponent(req.query['searchMovie']);
+  //console.log(movieParameter);
+
+  var searchMovie = apiUrl + movieParameter;
+
+  	request(searchMovie, function (error, response, body) {
+
+		  if (!error && response.statusCode == 200) {
+		    res.render('search', { movies: JSON.parse(body) });
+		  } else {
+		  	res.status(500).send('Something is wrong with the API !');
+		  }
+
+	});
 });
 
 module.exports = router;
